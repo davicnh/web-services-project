b1a91359d87b9cc5b6a20fdead253805d28c5227
Davi Miguel <davicunha910@gmail.com>
2026-09-01 08:49:51 -0300

    Web Service Project

---
+++ src/test/java/org/example/webservicesproject/WebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
