4600eedbeba9ff2f8976b77977c09841e2f506fd
Davi Miguel Cunha <davicunha910@gmail.com>
2026-09-02 16:22:34 -0300

    JPA and H2

--- src/main/java/org/example/webservicesproject/resources/UserResource.java
+++ src/main/java/org/example/webservicesproject/resources/UserResource.java
@@ -1,6 +1,6 @@
-package resources;
+package org.example.webservicesproject.resources;
 
-import entites.User;
+import org.example.webservicesproject.entites.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
